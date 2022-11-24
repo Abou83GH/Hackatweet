612cb8783571add467e8220785aad60b0d740ba8
GROMETTO N <nicolas.grometto@laposte.net>
2022-11-24 18:06:56 +0100

    reducer tweet in progress

--- frontend/components/ModaleUp.js
+++ frontend/components/ModaleUp.js
@@ -4,13 +4,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { changeModaleUp } from "../reducers/modaleUp";
 import { useDispatch } from "react-redux";
+import Link from 'next/link';
+import { useRouter } from "next/router";
 
 function ModaleUp() {
+
   const dispatch = useDispatch();
 
   const handleClose = () => {
     dispatch(changeModaleUp(false));
   };
+  
+  // fonctions pour changer de page
+  const router = useRouter();
+  console.log(router);
+  const navigate = () => {router.push('/homeTweet'); }
 
   return (
     <div className={styles.main}>
@@ -34,7 +42,8 @@ function ModaleUp() {
             type="password"
             placeholder="Password"
           />
-          <button className={styles.button}>Sign up</button>
+          <button className={styles.button} onClick={() => {navigate() }} >Sign up</button>
+          
         </div>
       </div>
     </div>
